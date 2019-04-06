4654852c32c07165a88ffb09dde0a97cc00d1bc1
Olivier ROLAND <newca12@users.noreply.github.com>
2019-04-06 15:44:44 +0200

    scala-native POC

--- src/main/scala/org/edla/port/atp/Main.scala
+++ src/main/scala/org/edla/port/atp/Main.scala
@@ -4,8 +4,20 @@ import org.edla.port.atp.Prop._
 import org.edla.port.atp.PropExamples.ramsey
 
 object Main extends {
+
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) + "ns")
+    result
+  }
+
+
   def main(args: Array[String]): Unit = {
     println("START")
-    println(tautology(ramsey(args(0).toInt, args(1).toInt, args(2).toInt)))
+    time {
+      println(tautology(ramsey(args(0).toInt, args(1).toInt, args(2).toInt)))
+    }
   }
 }
